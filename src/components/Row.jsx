4c88f18d9feef8e1c4cb57cf0d5d7724513f047c
Sumit Tripathi <sumit.tpt07@gmail.com>
2023-02-25 01:08:56 +0530

    Main component

---
+++ src/components/Row.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Movie from "./Movie";
+
+const Row = ({ title, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchUrl).then((res) => {
+      setMovies(res.data.results);
+    });
+  }, [fetchUrl]);
+
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center">
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide flex"
+        >
+          {movies.map((item, id) => {
+            return <Movie key={id} item={item} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Row;
